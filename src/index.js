d8c79f1e96d5a33e05f4b011718f5a820b1028d6
unknown <matheuscolombo@univem.edu.br>
2022-06-06 19:18:32 -0300

    adicionando cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const helmet =require("helmet");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
 require("./controllers/index")(app);
 
 app.listen(process.env.PORT || 3333);
